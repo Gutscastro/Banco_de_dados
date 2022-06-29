a2ef280b22a2edb7efcb218915397ab5f3b0feae
Gustavo de Castro <55390314+Gutscastro@users.noreply.github.com>
2022-06-29 09:49:04 -0300

    Pasta_TicketSystem
    Inclusão da pasta ticketSystem

---
+++ ticketsystem/application/helpers/swal_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+$status = false;
+
+   function doSwalAlert($title, $exp, $type){
+       $CI = &get_instance();
+       $status = true;
+
+       $CI->session->set_flashdata(array(
+         "title"  => $title,
+         "exp"    => $exp,
+         "type"   => $type,
+         "status" => $status
+       ));
+    }
